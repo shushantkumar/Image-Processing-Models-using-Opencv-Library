e56ea0a26b66f18eed36087133616c0d86415669
Shushant Kumar <shushantkmr2@gmail.com>
2018-02-02 03:53:28 -0800

    Update drawing_on_images.py

--- drawing_on_images.py
+++ drawing_on_images.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('p7.jpg',cv2.IMREAD_COLOR)			#reading image
+img = cv2.imread('pic.jpg',cv2.IMREAD_COLOR)			#reading image
 cv2.line(img,(0,0),(200,300),(255,255,255),50)		#drawing a line
 cv2.rectangle(img,(500,250),(1000,500),(0,0,255),15)	#drawing a rectangle
 cv2.circle(img,(447,63), 63, (0,255,0), -1)				#drawing a circle
